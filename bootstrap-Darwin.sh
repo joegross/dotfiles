53a5d1676c92460885f44d50e3265e0fa68013b0
Joe Gross <jgross@stimpy.net>
2014-11-18 20:29:33 -0800

    do not wipe iterm prefs

--- bootstrap-Darwin.sh
+++ bootstrap-Darwin.sh
@@ -6,6 +6,6 @@ rm -rf ~/Library/Application\ Support/Google/Android\ File\ Transfer/Android\ Fi
 rm -rf /Applications/Android\ File\ Transfer.app/Contents/Resources/Android\ File\ Transfer\ Agent.app
 pkill -f 'Android File Transfer Agent'
 
-launchctl stop com.apple.cfprefsd.xpc.agent
-cp osx/home/Library/Preferences/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
-launchctl start com.apple.cfprefsd.xpc.agent
+#launchctl stop com.apple.cfprefsd.xpc.agent
+#cp osx/home/Library/Preferences/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
+#launchctl start com.apple.cfprefsd.xpc.agent
