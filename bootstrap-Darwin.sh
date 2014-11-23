c6ed6068eb90adc37f41fd11b69a338f3a283892
Joe Gross <jgross@genability.com>
2014-11-23 13:31:21 -0800

    merge

--- bootstrap-Darwin.sh
+++ bootstrap-Darwin.sh
@@ -9,3 +9,5 @@ pkill -f 'Android File Transfer Agent'
 #launchctl stop com.apple.cfprefsd.xpc.agent
 #cp osx/home/Library/Preferences/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 #launchctl start com.apple.cfprefsd.xpc.agent
+
+brew install pdftex
