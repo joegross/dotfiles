f1eb169414e14760898d9c54d12711761706a3d7
Joe Gross <jgross@genability.com>
2015-01-13 18:35:48 -0800

    no pdftex

--- bootstrap-Darwin.sh
+++ bootstrap-Darwin.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 rm -rf ~/Library/Application\ Support/Google/Android\ File\ Transfer/Android\ File\ Transfer\ Agent.app
 rm -rf /Applications/Android\ File\ Transfer.app/Contents/Resources/Android\ File\ Transfer\ Agent.app
 pkill -f 'Android File Transfer Agent'
@@ -14,4 +12,4 @@ killall Dock
 #cp osx/home/Library/Preferences/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 #launchctl start com.apple.cfprefsd.xpc.agent
 
-brew install pdftex
+#brew install pdftex
