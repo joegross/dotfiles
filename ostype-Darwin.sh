a3ee99a94fa80f65d5584bd5a4fdcac1c7627bf4
Joe Gross <jgross@stimpy.net>
2015-06-16 22:37:04 -0700

    pip

--- ostype-Darwin.sh
+++ ostype-Darwin.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# kill hangy android file transfer agent
 rm -rf ~/Library/Application\ Support/Google/Android\ File\ Transfer/Android\ File\ Transfer\ Agent.app
 rm -rf /Applications/Android\ File\ Transfer.app/Contents/Resources/Android\ File\ Transfer\ Agent.app
 pkill -f 'Android File Transfer Agent'
 
 # kill dock bounces
-defaults write com.apple.dock no-bouncing -bool TRUE
-killall Dock
+if [ "$(defaults read com.apple.dock no-bouncing)" != "1" ]; then
+    defaults write com.apple.dock no-bouncing -bool TRUE
+    killall Dock
+fi
 
 #launchctl stop com.apple.cfprefsd.xpc.agent
 #cp osx/home/Library/Preferences/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist
 #launchctl start com.apple.cfprefsd.xpc.agent
 
 #brew install pdftex
+brew install emacs --cocoa --srgb # --with-gnutls
+brew install direnv
+brew install mercurial
+brew install pyenv
+brew install pyenv-virtualenv
+brew install pyenv-virtualenvwrapper
 brew install zsh-completions
 brew install zsh-history-substring-search
 brew install zsh-syntax-highlighting
-brew install direnv
-brew install emacs --cocoa --srgb # --with-gnutls
-brew install mercurial
 pip install --upgrade virtualenv
