308f5d7e3c8da61d6dca9ac89d24b06c1d687459
Joe Gross <jgross@stimpy.net>
2015-07-29 17:03:30 -0700

    remove mouse

--- emacs.d/settings/general-settings.el
+++ emacs.d/settings/general-settings.el
@@ -36,18 +36,18 @@
 ;(setq mouse-sel-mode t)
 
 ;; Enable mouse support
-(unless window-system
-  (require 'mouse)
-  (xterm-mouse-mode t)
-  (global-set-key [mouse-4] '(lambda ()
-                               (interactive)
-                               (scroll-down 1)))
-  (global-set-key [mouse-5] '(lambda ()
-                               (interactive)
-                               (scroll-up 1)))
-  (defun track-mouse (e))
-  (setq mouse-sel-mode t)
-  )
+;(unless window-system
+;  (require 'mouse)
+;  (xterm-mouse-mode t)
+;  (global-set-key [mouse-4] '(lambda ()
+;                               (interactive)
+;                               (scroll-down 1)))
+;  (global-set-key [mouse-5] '(lambda ()
+;                               (interactive)
+;                               (scroll-up 1)))
+;  (defun track-mouse (e))
+;  (setq mouse-sel-mode t)
+;  )
 
 ; set command key to be meta instead of option
 (if (system-is-mac)
