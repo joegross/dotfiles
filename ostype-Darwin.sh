3d05130f0c66ce033c7f4f336be6ffa5764ead6d
Joe Gross <jgross@stimpy.net>
2015-11-06 11:27:06 -0800

    install brew-cask

--- ostype-Darwin.sh
+++ ostype-Darwin.sh
@@ -31,6 +31,7 @@ brew install zsh-completions
 brew install zsh-history-substring-search
 brew install zsh-syntax-highlighting
 brew install brew-cask
+brew install caskroom/cask/brew-cask
 brew cask install haskell-platform
 pip install --upgrade virtualenv pyflakes
 
