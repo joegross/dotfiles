077238ea098b04485ed4349109447fa84b525017
Joe Gross <jgross@stimpy.net>
2016-06-05 20:57:40 -0700

    read lines in a pedantic line-oritented way and other shellcheck fixes

--- ostype-Darwin.sh
+++ ostype-Darwin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # kill hangy android file transfer agent
-rm -rf ~/Library/Application\ Support/Google/Android\ File\ Transfer/Android\ File\ Transfer\ Agent.app
-rm -rf /Applications/Android\ File\ Transfer.app/Contents/Resources/Android\ File\ Transfer\ Agent.app
+rm -rf "$HOME/Library/Application Support/Google/Android File Transfer/Android File Transfer Agent.app"
+rm -rf "/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent.app"
 pkill -f 'Android File Transfer Agent'
 
 # kill dock bounces
@@ -20,13 +20,20 @@ defaults write com.apple.coreservices.uiagent CSUIHasSafariBeenLaunched -bool YE
 defaults write com.apple.coreservices.uiagent CSUIRecommendSafariNextNotificationDate -date 2050-01-01T00:00:00Z
 defaults write com.apple.coreservices.uiagent CSUILastOSVersionWhereSafariRecommendationWasMade -float 10.99
 
-brew install $(cat brew-packages.txt)
+grep -v '^ *#' < brew-packages.txt | while IFS= read -r line
+do
+  brew install "$line"
+done
 brew install emacs --cocoa --srgb # --with-gnutls
+# brew link --overwrite ghc cabal-install
 
 # iterm shell integration
-curl -sL https://iterm2.com/misc/zsh_startup.in -o $HOME/.iterm2_shell_integration.zsh
+curl -sL https://iterm2.com/misc/zsh_startup.in -o "$HOME/.iterm2_shell_integration.zsh"
 
-brew cask install $(cat brew-cask-packages.txt)
+grep -v '^ *#' < brew-cask-packages.txt | while IFS= read -r line
+do
+  brew cask install "$line"
+done
 
 pip install --upgrade -r requirements.txt
 
@@ -34,7 +41,7 @@ pip install --upgrade -r requirements.txt
 
 # zsh-git-prompt
 # compile gitstatus (haskell)
-cd $HOME/dev/zsh-git-prompt
+cd "$HOME/dev/zsh-git-prompt" || exit
 stack setup
 stack build
 stack install
