70f2a69ee72865d38f1ea2990e22e827beb73a3e
Joe Gross <jgross@stimpy.net>
2017-09-10 22:18:22 -0700

    use brewfile, let dropbox be dropbox, android agent not in use, try -e

--- ostype-Darwin.sh
+++ ostype-Darwin.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # kill hangy android file transfer agent
-rm -rf "$HOME/Library/Application Support/Google/Android File Transfer/Android File Transfer Agent.app"
-rm -rf "/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent.app"
-pkill -f 'Android File Transfer Agent'
+# rm -rf "$HOME/Library/Application Support/Google/Android File Transfer/Android File Transfer Agent.app"
+# rm -rf "/Applications/Android File Transfer.app/Contents/Resources/Android File Transfer Agent.app"
+# pkill -f 'Android File Transfer Agent'
 
 # kill dock bounces
 if [ "$(defaults read com.apple.dock no-bouncing)" != "1" ]; then
@@ -20,9 +22,9 @@ defaults write com.apple.coreservices.uiagent CSUIHasSafariBeenLaunched -bool YE
 defaults write com.apple.coreservices.uiagent CSUIRecommendSafariNextNotificationDate -date 2050-01-01T00:00:00Z
 defaults write com.apple.coreservices.uiagent CSUILastOSVersionWhereSafariRecommendationWasMade -float 10.99
 
-mkdir -p $HOME/screenshots
-defaults write com.apple.screencapture location $HOME/Dropbox/screenshots
-killall SystemUIServer
+# mkdir -p $HOME/screenshots
+# defaults write com.apple.screencapture location $HOME/Dropbox/screenshots
+# killall SystemUIServer
 
 # I never want apple photos
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
@@ -31,6 +33,9 @@ defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
 if ! which brew > /dev/null; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+brew update
+brew bundle
 
 (cd "ansible" && ./ansible.sh)
 
